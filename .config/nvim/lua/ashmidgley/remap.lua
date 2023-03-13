ce3de0fbcfda14c224f6353018fc6589aeed3915
Ash Midgley <ashleymidgley@protonmail.com>
2023-03-13 17:20:43 +1300

    Add Harpoon.

--- .config/nvim/lua/ashmidgley/remap.lua
+++ .config/nvim/lua/ashmidgley/remap.lua
@@ -1,5 +1,7 @@
+-- Explore remap.
 vim.keymap.set("n", "<leader>v", vim.cmd.Ex)
 
+-- Highlight + move remap.
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- .config/nvim/lua/ashmidgley/set.lua
+++ .config/nvim/lua/ashmidgley/set.lua
@@ -1,6 +1,12 @@
+-- Gruvbox theme.
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+
+-- line numbers + relative line numbers.
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Tabs, tabs, tabs.
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -8,22 +14,25 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
+-- No line-wrap.
 vim.opt.wrap = false
 
+-- No vim backup but save long-running undo's.
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+vim.opt.updatetime = 50
 
+-- Incremental search highlighting.
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- Good colors.
 vim.opt.termguicolors = true
 
+-- Minimum number of lines at bottom of file.
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
-
+-- Color column width.
 vim.opt.colorcolumn = "80"
